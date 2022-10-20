e073f194e86539eaf210b150c3f57663279ac8f1
loebfly <357406094@qq.com>
2022-10-20 11:10:55 +0800

    i18n CheckRes 修改

--- ezi18n.go
+++ ezi18n.go
@@ -115,7 +115,7 @@ func (receiver I18nStringId) ErrorResWithStatusAndArgs(status int, args ...inter
 func (receiver I18nStringId) CheckRes(params map[string]string, key ...string) engine.Result {
 	for _, param := range key {
 		if _, ok := params[param]; !ok {
-			return receiver.ErrorResWithMsg(param, 1003)
+			return receiver.ErrorResWithArgs(param, 1003)
 		}
 	}
 	return receiver.Result(nil)
