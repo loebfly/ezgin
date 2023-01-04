841269b127bedb3850eb6412e7fdfce26903bfd4
loebfly <357406094@qq.com>
2023-01-04 11:25:54 +0800

    跨域允许所有请求头

--- internal/engine/middleware/cors/cors.go
+++ internal/engine/middleware/cors/cors.go
@@ -9,7 +9,12 @@ import (
 func (enter) Middleware(ctx *gin.Context) {
 	ctx.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 	ctx.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	ctx.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	allowHeaders := ""
+	// 允许所有请求头
+	for k := range ctx.Request.Header {
+		allowHeaders += k + " ,"
+	}
+	ctx.Writer.Header().Set("Access-Control-Allow-Headers", allowHeaders)
 	ctx.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 	if ctx.Request.Method == "OPTIONS" {
 		ctx.AbortWithStatus(http.StatusNoContent)
